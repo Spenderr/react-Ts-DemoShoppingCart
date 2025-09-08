4fbef65247c75c0ce3c44592cd23d0947723a7c8
Spender <ahmed.patel@ogr.deu.edu.tr>
2025-09-08 17:29:24 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-Ts-DemoShoppingCart/shopping-cart", 
+  base: "/react-Ts-DemoShoppingCart/",
+  build: {
+    outDir: "dist", // default
+  },
 });
