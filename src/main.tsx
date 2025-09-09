a1a64548bd21f924afbb27036ea41b64b3338b0b
Spender <ahmed.patel@ogr.deu.edu.tr>
2025-09-09 13:37:23 +0300

    Fix GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { CartProvider } from './context/CartContext';
-import { BrowserRouter } from 'react-router-dom';
+
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <BrowserRouter basename="/react-Ts-DemoShoppingCart/">
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
 }
