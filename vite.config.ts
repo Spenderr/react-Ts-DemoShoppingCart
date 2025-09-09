7a243daba4d7b01816505d50f7986054a4466110
Spender <ahmed.patel@ogr.deu.edu.tr>
2025-09-09 13:32:34 +0300

    m

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-Ts-DemoShoppingCart/", 
+  base: "./", 
   build: {
-    outDir: "docs", // build into docs folder
+    outDir: "docs",
   },
 });
