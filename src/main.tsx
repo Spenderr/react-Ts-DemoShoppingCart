dafef0f37f01db370d2045663afa735c0c859aca
Spender <ahmed.patel@ogr.deu.edu.tr>
2025-09-09 13:25:45 +0300

    s

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { CartProvider } from './context/CartContext';
-
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </BrowserRouter>
     </React.StrictMode>
   );
 } else {
